860162278099db4b1ff1a3c4ad970625502f71ea
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-04-21 14:27:32 +0200

    Add commands to approve and retrieve certificate for Alice

--- exercises/exercise-7.sh
+++ exercises/exercise-7.sh
@@ -7,3 +7,7 @@ openssl req -new -key alice.key -out alice.csr -subj "/CN=alice"
 cat alice.csr | base64 | tr -d "\n"
 
 kubectl apply -f alice-csr.yaml
+
+kubectl certificate approve alice
+
+kubectl get csr alice -o jsonpath='{.status.certificate}' | base64 -d > alice.crt
